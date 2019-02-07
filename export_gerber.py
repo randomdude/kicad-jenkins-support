e3b3953bc78dba8857081fbf2c756fcc71707cbc
aliz <aliz>
2019-02-08 01:18:26 +0800

    correct output dir for drillfiles

--- export_gerber.py
+++ export_gerber.py
@@ -50,5 +50,5 @@ for boardfile in glob.glob("*.kicad_pcb"):
 
 	genDrl = True
 	genMap = False
-	drlwriter.CreateDrillandMapFilesSet( "gerbers", genDrl, genMap );
+	drlwriter.CreateDrillandMapFilesSet( "generated", genDrl, genMap );
 
