f3daaf167f829f922dab112f634cf14f71ad483f
aliz <aliz>
2019-02-10 21:33:34 +0800

    add versioning.py

---
+++ versioning.py
@@ -0,0 +1,26 @@
+import os
+import sys
+import glob
+
+if len(sys.argv) != 3:
+	print "Usage: %s [git hash] [build number]" % (sys.argv[0])
+	sys.exit(-1)
+
+githash     = "%05d" % int(sys.argv[1])
+buildnumber = "%03d" % int(sys.argv[2])
+
+pcbfiles = glob.glob("*.kicad_pcb")
+if len(pcbfiles) == 0:
+	raise Exception("No PCBs found")
+
+for pcbfile in pcbfiles:
+	pcbfile = os.path.join( os.getcwd(), pcbfile)
+
+	with open(pcbfile, 'r') as file :
+		filedata = file.read()
+
+	filedata = filedata.replace('GIT_XXXXX', 'GIT_' + githash)
+	filedata = filedata.replace('BUILD_XXX', 'BUILD_' + buildnumber)
+
+	with open(pcbfile, 'w') as file:
+		file.write(filedata)
