1afaee8e1e3876c811f4216ee8dc83ffae10f298
aliz <aliz>
2022-04-16 05:34:28 +0800

    fix path for kicad 6.0

--- build.groovy
+++ build.groovy
@@ -6,13 +6,13 @@ def build(mainRepoPath, boardDir = '.') {
     bat script: "copy mainCheckout\\${boardDir}\\* ."
     
     buildNo = mainCheckout.GIT_COMMIT.substring(0, 5)
-    bat script: "\"c:\\Program Files\\KiCad\\bin\\python.exe\" versioning.py ${buildNo} ${BUILD_NUMBER}"
+    bat script: "\"c:\\Program Files\\KiCad\\6.0\\bin\\python.exe\" versioning.py ${buildNo} ${BUILD_NUMBER}"
     
-    bat script: '''\"c:\\Program Files\\KiCad\\bin\\python.exe\" export_SVG.py'''
+    bat script: '''\"c:\\Program Files\\KiCad\\6.0\\bin\\python.exe\" export_SVG.py'''
     bat script: "refreshenv && magick mogrify -format png -trim -border 10 *.svg"
     archiveArtifacts artifacts: '*.png', caseSensitive: false, defaultExcludes: false
 
-    bat script: '''\"c:\\Program Files\\KiCad\\bin\\python.exe\" export_gerber.py'''
+    bat script: '''\"c:\\Program Files\\KiCad\\6.0\\bin\\python.exe\" export_gerber.py'''
     gerberFilename = "${JOB_NAME}_${BUILD_NUMBER}.zip"
     zip zipFile: gerberFilename, archive: false, dir: 'generated'
     archiveArtifacts gerberFilename
