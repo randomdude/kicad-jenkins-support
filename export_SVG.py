3f9c85afec1b83973cd027e163fcf3b60d55dbe4
aliz <aliz>
2022-04-16 10:03:44 +0800

    correct PCB file extension

--- export_SVG.py
+++ export_SVG.py
@@ -10,7 +10,7 @@ from svg_processor import SvgProcessor
 # The idea is to export each layer to an SVG, then change the colour of each, and finally
 # combine them all into one layer. This is because KiCad won't change colours on svg export.
 
-for boardfile in Path(".").rglob("*.kicad_pro"):
+for boardfile in Path(".").rglob("*.kicad_pcb"):
 	board = pcbnew.LoadBoard(str(boardfile.absolute()))
 	svgfilename = str(boardfile.absolute()) + ".svg"
 
